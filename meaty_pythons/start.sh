922a2731c7ec9b634711862ff270a00fe4327030
Colm_Rooney <77832627+CFR2000@users.noreply.github.com>
2023-05-19 03:45:53 +0100

    incorporate redis waiting

--- meaty_pythons/start.sh
+++ meaty_pythons/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Start the server and pass in the host name and port environment variables
-python /app/db/init.py --host=$HOSTNAME --port=$PORT
+/app/src/wait-for-redis.sh $REDIS_HOST -- python /app/db/init.py --host=$REDIS_HOST --port=$REDIS_PORT
 python /app/src/resolvers.py
