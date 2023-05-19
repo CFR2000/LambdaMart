3590351348fda0991225f0af8b426278755543ea
Colm_Rooney <77832627+CFR2000@users.noreply.github.com>
2023-05-19 03:45:36 +0100

    script to wait for redis

---
+++ meaty_pythons/src/wait-for-redis.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# wait-for-redis.sh
+
+set -e
+
+host="$1"
+shift
+cmd="$@"
+
+until redis-cli -h "$host" ping; do
+  >&2 echo "Redis is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Redis is up - executing command"
+exec $cmd
