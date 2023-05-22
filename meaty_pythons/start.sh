6cd77c85b205907f9a8d68dc901f8acc65227b01
Colm_Rooney <77832627+CFR2000@users.noreply.github.com>
2023-05-22 18:07:37 +0100

    moving inside src

--- meaty_pythons/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Set permissions for the script
-chmod +x /app/src/wait-for-redis.sh
-# Start the server and pass in the host name and port environment variables
-/app/src/wait-for-redis.sh $REDIS_HOST -- python /app/db/init.py --host=$REDIS_HOST --port=$REDIS_PORT
-python /app/src/resolvers.py
