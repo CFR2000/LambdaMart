0e67ccd3ea8298c0b3e0ce856e09fb484fa09239
Ben Brown <62847149+arealglitterboy@users.noreply.github.com>
2023-05-31 00:22:55 +0100

    Added vendors page (whoops)

--- lambda-mart/src/pages/vendors.tsx
+++ lambda-mart/src/pages/vendors.tsx
@@ -2,23 +2,121 @@ import React from "react";
 
 import Layout from "../layouts/page-layout";
 
-import { Heading, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Card,
+  CardBody,
+  CardFooter,
+  Divider,
+  Heading,
+  Spinner,
+  Stack,
+  Text,
+  Image,
+  useToast,
+  SimpleGrid,
+} from "@chakra-ui/react";
+import { gql, useQuery } from "@apollo/client";
+import { Vendor } from "../types";
+
+const VendorCard = ({
+  icon,
+  title,
+  description,
+  toast,
+}: Vendor & { toast: any }) => (
+  <Card maxW="sm">
+    <CardBody>
+      <Image src={icon} alt={`The icon for {title}`} borderRadius="lg" />
+      <Stack mt="6" spacing="3">
+        <Heading size="md">{title}</Heading>
+        <Text>{description}</Text>
+      </Stack>
+    </CardBody>
+    <Divider />
+    <CardFooter>
+      <ButtonGroup spacing="2">
+        <Button
+          variant="solid"
+          colorScheme="blue"
+          onClick={toast({
+            title: "Clicked",
+            description: "That was fun wasn't it?",
+            status: "success",
+            duration: 900,
+            isClosable: true,
+            colorScheme: "primary",
+          })}
+        >
+          Click a button?
+        </Button>
+        <Button
+          variant="ghost"
+          colorScheme="danger"
+          onClick={toast({
+            title: "WHAT DID YOU DO?!",
+            description: "You took down the website!",
+            status: "error",
+            duration: 9000,
+            isClosable: true,
+            colorScheme: "secondary",
+          })}
+        >
+          Take down website
+        </Button>
+      </ButtonGroup>
+    </CardFooter>
+  </Card>
+);
 
 const Vendors = () => {
+  const { data, loading } = useQuery(
+    gql`
+      query Vendors {
+        vendors {
+          description
+          icon
+          title
+          vendorId
+        }
+      }
+    `,
+    {
+      pollInterval: 0,
+    }
+  );
+
+  const toast = useToast();
+  toast({
+    title: "Account created.",
+    description: "We've created your account for you.",
+    status: "success",
+    duration: 9000,
+    isClosable: true,
+    colorScheme: "primary",
+  });
   return (
     <Layout>
-      <Heading as="h1">Vendors</Heading>
+      <Box maxW="7xl" mx="auto" py="md">
+        <Heading as="h1">Vendors</Heading>
 
-      <Text>Please implement me sire</Text>
+        {loading ? (
+          <Spinner mx="auto" h="100%" />
+        ) : (
+          data?.vendors?.map((vendor: Vendor) => (
+            <SimpleGrid
+              spacing={4}
+              templateColumns="repeat(auto-fill, minmax(200px, 1fr))"
+            >
+              <VendorCard key={vendor.vendorId} {...vendor} toast={toast} />
+            </SimpleGrid>
+          ))
+        )}
+      </Box>
     </Layout>
   );
 };
 
 export default Vendors;
-
-// export const query = graphql`
-//   query VendorsPageQuery {
-//     broker {
-//       vendors {
-
-//     }
